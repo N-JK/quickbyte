9e078935772b900a771c493a12e8d733795d3b46
Nibin <nibinjohn29@gmail.com>
2025-03-07 11:29:18 +0530

    docker1

--- quickbyte/settings.py
+++ quickbyte/settings.py
@@ -82,10 +82,21 @@ WSGI_APPLICATION = "quickbyte.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Njkqwerty1234567890',
+        'HOST': 'quickbytedb2.clkw0i8agn25.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
